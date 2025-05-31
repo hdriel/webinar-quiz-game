8320d3bb37873185668e4721c08a83c7e835cae8
hdriel <hdriel@gmail.com>
2025-05-31 22:09:53 +0300

    add data state

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import DATA from './assets/data.json';
+import { useEffect, useState } from 'react';
+import type { QUIZ } from './interfaces/QUIZ.ts';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [data, setData] = useState<QUIZ[]>([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        // fetch('')
+        //     .then((res) => res.json)
+        //     .then((data) => setData(data));
+        setData(DATA as QUIZ[]);
+    }, []);
+
+    return <pre>{JSON.stringify(data, null, 4)}</pre>;
 }
 
-export default App
+export default App;
